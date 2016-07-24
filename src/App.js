86ca13175220f5ab0ea4e8ca8a2887f60007fa4a
Neil Renicker <neilrenicker@gmail.com>
2016-07-24 16:18:15 -0500

    feat: add alphabet list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import LETTERS from './constants/letters';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>stranger things</h1>
+        <ul className="list-reset List">
+          {LETTERS.map((letter, index) => {
+            return (
+              <li
+                className="ListItem bold inline-block p1 caps"
+                key={index}
+                >
+                {letter}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
